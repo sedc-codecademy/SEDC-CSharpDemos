bdc52bb19ab4e604b0d975e015ef7fa51508bd68
sweko <swekster@gmail.com>
2015-02-04 18:47:16 +0100

    04.02 Class

---
+++ DigitsFinder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitsFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number ");
+            var line = Console.ReadLine();
+
+            int number;
+            var check = int.TryParse(line, out number);
+            if (!check)
+            {
+                Console.WriteLine("{0} is not a valid number", line);
+                return;
+            }
+
+            var digits = new List<int>();
+
+            while (number != 0)
+            {
+                int cifra = number%2;
+                number /= 2;
+                digits.Add(cifra);
+            }
+
+            //for (int i = digits.Count -1 ; i >=0; i--)
+            //{
+            //    Console.WriteLine(digits[i]);
+            //}
+
+            //for (int i = 0; i < digits.Count; i++)
+            //{
+            //    var index = digits.Count - i - 1;
+            //    Console.WriteLine(digits[index]);
+            //}
+
+            digits.Reverse();
+            foreach (var digit in digits)
+            {
+                Console.Write("{0}",digit);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MethodsStart/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = Add(1, 2);
+            Console.WriteLine(result1);
+
+            var result2 = Add(1.1, 2.3);
+            Console.WriteLine(result2);
+
+            var result3 = Add(1.1, "Petar");
+            Console.WriteLine(result3);
+
+        }
+
+        public static double Add(double x, double y)
+        {
+            Console.WriteLine("Double Add Called");
+            return x + y;
+        }
+
+        public static int Add(int x, int y)
+        {
+            Console.WriteLine("Int Add Called");
+            return x + y;
+        }
+
+        public static string Add(double x, string y)
+        {
+            Console.WriteLine("DS Add Called");
+            return x.ToString() +" "+ y;
+        }
+
+        public static void AddPrint(double x, string y)
+        {
+            Console.WriteLine("DS Add Called");
+            return;
+        }
+    }
+
+}
